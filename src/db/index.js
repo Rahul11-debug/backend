5faaf79e868dcc1275f04e67d90219f8aea41566
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2025-10-07 12:36:41 +0530

    connected to mongo atlas

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log("MONGO_URI:", process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
