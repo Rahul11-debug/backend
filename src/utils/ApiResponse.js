5e801631371d04dbc2bc8476a85717e192e1e94a
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2025-10-07 14:57:49 +0530

    update utils

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,9 +1,9 @@
 class ApiResponse {
   constructor(success, data,message="success") {
     this.statusCode = statusCode
-    this.success = statusCode < 400;
     this.message = message;
     this.data = data;  
+    this.success = statusCode < 400;
   }
 }
 export { ApiResponse };
